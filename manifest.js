9ae53c231f4041c58cd7ba640418a06a9295eb4a
BurbanOfficial <burban.official@outlook.com>
2025-03-14 22:43:21 +0100

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Burban",
+    "short_name": "Burban",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#00bcd4",
+    "icons": [
+      {
+        "src": "https://burbanofficial.com/favicon.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "https://burbanofficial.com/favicon.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
+  
